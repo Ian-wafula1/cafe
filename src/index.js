cd2301f063d1df9ed522889abd51c9f4a4d1ff87
Ian Wafula <neocorpse@proton.me>
2025-04-02 21:26:55 +0300

    Add and style contact section

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import "./reset/modern-normalize.css"
 import "./reset/custom-reset.css";
 import "./style.css"
 
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault()
+})
 console.log('it works!')
