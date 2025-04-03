640c5333e2d10dc8767f37aef02997c8e5a3241b
Ian Wafula <neocorpse@proton.me>
2025-04-03 08:06:54 +0300

    Convert about section to javascript

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import "./styles/reset/custom-reset.css";
 import "./styles/style.css"
 import loadHome from './scripts/home'
 import loadMenu from "./scripts/menu";
+import loadAbout from "./scripts/about";
 
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault()
 })
 
-loadMenu()
+loadAbout()
--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,1 +1,13 @@
-import { createDiv, createElement } from "./helpers";
+import { createParent, createElement } from "./helpers";
+
+export default function loadAbout() {
+    const mainContent = document.querySelector('#content')
+    mainContent.innerHTML = ''
+    const main = document.createElement('main')
+    main.classList.add('about')
+
+    main.appendChild(createElement('p', `Nestled in the heart of the city, our café is a cozy retreat where rich aromas and warm ambiance create the perfect escape. We take pride in serving expertly crafted coffee, brewed from the finest beans, alongside a menu of fresh, locally sourced delights. Whether you're here for a quiet morning moment, a midday pick-me-up, or an evening unwind, our space is designed for comfort and connection. With a passion for quality and community, we invite you to relax, sip, and savor every experience. Step in and let the aroma of fresh coffee welcome you home. From the first pour to the last bite, every detail is thoughtfully prepared to delight your senses. Come and discover a place where every visit feels like a warm embrace.`))
+
+    main.appendChild(createElement('div', ''))
+    mainContent.appendChild(main)
+}
